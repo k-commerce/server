d2a2609e276e45907aa1a232ad3bfa20ba45ea6e
kwondongwook <kwondongwook1@gmail.com>
2023-03-10 21:30:10 +0900

    first commit

---
+++ src/main/java/com/kcommerce/KCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.kcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcommerce/KCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
