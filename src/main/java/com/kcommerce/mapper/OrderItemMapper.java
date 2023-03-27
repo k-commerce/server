26e32263991a151076df242dcb45bf1fee7340d3
YeongHun-KO <zglz1997@gmail.com>
2023-03-27 23:30:56 +0900

    CHORE : Mapper Class -> Mapstruct Interface로 변경

--- src/main/java/com/kcommerce/mapper/OrderItemMapper.java
+++ src/main/java/com/kcommerce/mapper/OrderItemMapper.java
@@ -1,35 +1,20 @@
 package com.kcommerce.mapper;
 
 import com.kcommerce.domain.OrderItem;
+import com.kcommerce.dto.ItemDto;
+import com.kcommerce.dto.OrderDto;
 import com.kcommerce.dto.OrderItemDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-import java.util.List;
-import java.util.stream.Collectors;
+@Mapper(componentModel = "spring")
+public interface OrderItemMapper {
 
-@Component
-@RequiredArgsConstructor
-public class OrderItemMapper {
-
-    private final OrderMapper orderMapper;
-    private final ItemMapper itemMapper;
-
-    public OrderItemDto.Response toDto(OrderItem orderItem) {
-        return new OrderItemDto.Response(
-                orderItem.getId(),
-                orderMapper.toDto(orderItem.getOrder()),
-                itemMapper.toDto(orderItem.getItem()),
-                orderItem.getStatus(),
-                orderItem.getQuantity(),
-                orderItem.getOrderPrice()
-        );
-    }
-
-    public List<OrderItemDto.Response> toDtoList(List<OrderItem> orderItemList) {
-        return orderItemList
-                .stream()
-                .map(this::toDto)
-                .collect(Collectors.toList());
-    }
+     @Mapping(target = "id", source = "orderItem.id")
+     @Mapping(target = "order", source = "orderDto")
+     @Mapping(target = "item", source = "itemDto")
+     @Mapping(target = "status", source = "orderItem.status")
+     @Mapping(target = "quantity", source = "orderItem.quantity")
+     @Mapping(target = "orderPrice", source = "orderItem.orderPrice")
+     OrderItemDto.Response toDto(OrderItem orderItem, OrderDto.Response orderDto, ItemDto.Response itemDto);
 }
