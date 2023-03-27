126cad44223d95323c0a5239c3db1a708fb93e36
YeongHun-KO <zglz1997@gmail.com>
2023-03-27 21:08:22 +0900

    CHORE : OrderDto Request Validation 추가

--- src/main/java/com/kcommerce/dto/OrderDto.java
+++ src/main/java/com/kcommerce/dto/OrderDto.java
@@ -18,8 +18,14 @@ public class OrderDto {
     @Getter
     @Setter
     public static class Request {
+
+        @NotBlank
         private PaymentType payment;
+
+        @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎㅏ-ㅣ가-힣]{1,9}$", message = "이름은 1~9자의 영문, 숫자, 한글만 사용 가능합니다.")
         private String name;
+
+        @Pattern(regexp = "^[0-9]{11}$", message = "휴대전화번호는 11자의 숫자만 사용 가능합니다.")
         private String phoneNumber;
 
         @Pattern(regexp = "^[0-9]{5}$", message = "우편번호는 5자의 숫자로 입력해 주세요.")
@@ -32,8 +38,13 @@ public class OrderDto {
         @Size(max = 100, message = "상세 주소는 100자 이하로 입력해 주세요.")
         private String detailed;
 
+        @NotBlank
         private List<Long> itemIdList;
+
+        @NotBlank
         private List<Integer> countList;
+
+        @NotBlank
         private List<Integer> totalPriceList;
     }
 
