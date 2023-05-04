b6f57970f7638a3685eed97214658d0b7bb307b8
kwondongwook <kwondongwook1@gmail.com>
2023-05-04 16:54:05 +0900

    CHORE : @EnableJpaAuditing JpaConfig로 이동

--- src/main/java/com/kcommerce/KCommerceApplication.java
+++ src/main/java/com/kcommerce/KCommerceApplication.java
@@ -2,10 +2,8 @@ package com.kcommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class KCommerceApplication {
 
     public static void main(String[] args) {
